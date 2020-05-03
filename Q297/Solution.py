9d86770cde3eb23a5d5e3a400d94723fbf8f4935
JiananDing0 <47720443+JiananDing0@users.noreply.github.com>
2020-05-03 01:45:48 -0400

    Update Solution.py

--- Q297/Solution.py
+++ Q297/Solution.py
@@ -13,7 +13,7 @@ class Codec:
         :rtype: str
         """
         queue = [root]
-        result = ''
+        result = []
         # When the queue get all 'null', we quit
         determine = lambda lst: all(x is None for x in lst)
         # Breadth first search
@@ -23,40 +23,37 @@ class Codec:
             queue.pop(0)
             # Append value to result list
             if currNode is None:
-                result += 'null'
+                result.append('null')
             else:
-                result += str(currNode.val)
+                result.append(str(currNode.val))
                 # Renew the queue
                 queue.append(currNode.left)
                 queue.append(currNode.right)
-            result += ','
-        if result == '':
-            return '[' + result + ']'
-        return '[' + result[:-1] + ']'
+        # List to string
+        return ','.join(result)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
         :type data: str
         :rtype: TreeNode
         """
+        # Preprocessing the string data into a list of data
+        if data == '':
+            return None
+        dataList = data.split(",")
+        # Find the return values
+        return self.deserialize_helper(dataList, 0)
+    
+    def deserialize_helper(self, data, index):
         # Pre-defined functions
         leftNode = lambda i: i * 2 + 1
         rightNode = lambda i: i * 2 + 2
-        # Preprocessing the string data into a list of data
-        if len(data) == 2:
-            return None
-        data = data[1:-1]
-        dataList = data.split(",")
-        # Find the return values
-        return self.deserialize_helper(dataList, 0, leftNode, rightNode)
-    
-    def deserialize_helper(self, data, index, leftNode, rightNode):
         # Depth First Search
         if index >= len(data):
             return None
         newNode = TreeNode(data[index])
-        newNode.left = self.deserialize_helper(data, leftNode(index), leftNode, rightNode)
-        newNode.right = self.deserialize_helper(data, rightNode(index), leftNode, rightNode)
+        newNode.left = self.deserialize_helper(data, leftNode(index))
+        newNode.right = self.deserialize_helper(data, rightNode(index))
         return newNode
 
 # Your Codec object will be instantiated and called as such:
